d8db48df920bb237679b8c790a2ca5f89cb036f7
Duy Khoi Nguyen <duy.nguyen2@stud.hs-flensburg.de>
2024-04-18 10:38:15 +0200

    init project

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <>
+      <section>
+        <article>
+          <p>
+            {' '}
+            Portfolio is under construction.
+            <a href='https://portfolio-sigma-rouge-53.vercel.app/'>
+              Here you can view the old Portfolio
+            </a>
+          </p>
+        </article>
+      </section>
+    </>
+  );
+};
+
+export default Header;
